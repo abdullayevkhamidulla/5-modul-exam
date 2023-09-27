9536725bb67c0694e2fdfc29cda182e3f435656e
abdullayevkhamidullo <you@example.com>
2023-09-27 12:53:48 +0500

    last

--- main.js
+++ main.js
@@ -2,6 +2,9 @@ let boxes = document.querySelectorAll(".box");
 let right = document.getElementById("right");
 let left = document.getElementById("left");
 let center = document.getElementById("center");
+let addtaskbutton = document.getElementById("btn1");
+let addtaskbutton1 = document.getElementById("btn2");
+let addtaskbutton2 = document.getElementById("btn3");
 
 
 
@@ -35,6 +38,30 @@ for (box of boxes){
         center.appendChild(selected)
         selected = null;
     })
-})
+    
+   
 
+});
 }
+addtaskbutton.addEventListener("click", ()=> {
+    const card =document.createElement("div");
+    card.className="list h2";
+    card.textContent="exra"
+    left.append(card);
+    card.draggable=true;
+});
+addtaskbutton1.addEventListener("click", ()=> {
+    const card =document.createElement("div");
+    card.className="list h2";
+    card.textContent="exra"
+    center.append(card);
+    card.draggable=true;
+});
+addtaskbutton2.addEventListener("click", ()=> {
+    const card =document.createElement("div");
+    card.className="list h2";
+    card.textContent="exra"
+    right.append(card);
+    card.draggable=true;
+});
+
